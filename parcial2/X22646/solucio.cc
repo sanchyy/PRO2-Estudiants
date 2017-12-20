26feb376cc0bbca9dfca9fc968bfae601f6862ee
Victor Sanchez Gassull <victor.sanchez.gassull@b5s201pc23.fib.upc.es>
2017-12-20 10:48:57 +0100

    primer exercici punters1 i parcial2

--- parcial2/X22646/solucio.cc
+++ parcial2/X22646/solucio.cc
@@ -2,5 +2,24 @@
 /* Pre: p.i. = C1, c = C2 */ 
 /* Post: el p.i. passa a ser el resultat de trenar C1 i C2; c passa a ser buida */
 void trenat(Cua &c) {
-	
+	//demanar_torn = push
+	//avancar = pop
+	//primer = front
+	//es_buida = empty
+	node_cua* prim = this->primer_node;
+	node_cua* cnode = c.primer_node;
+	if (prim == NULL) this->primer_node = cnode; //si el primer node del paràmetre implicit és nul, tota la cua serà c.
+	if (this->longitud <= c.longitud) ultim_node = c.ultim_node;
+	this->longitud += c.longitud;
+   
+	while (prim != NULL and cnode != NULL) {
+			node_cua* auxprim = prim->seguent;
+			node_cua* auxc = cnode->seguent;
+			prim->seguent = cnode;
+			if (auxprim != NULL) {
+				cnode->seguent = auxprim;
+			}
+			prim = auxprim;
+			cnode = auxc;
+	}
 }
--- punters1/X54670/program.cc
+++ punters1/X54670/program.cc
@@ -1,11 +1,18 @@
-#include <iostream>
-#include <queue>
-#include "CuaIOint.hh"
-#include "Cua.hh"
-using namespace std;
 
 
 
-int main() {
-
+void concat(Cua &c) {
+    if (longitud == 0) {
+        primer_node = c.primer_node;
+        ultim_node = c.ultim_node;
+        longitud = c.longitud;
+    }
+    else if (c.longitud > 0) {
+        ultim_node->seguent = c.primer_node;
+        ultim_node = c.ultim_node;
+        longitud += c.longitud;
+    }
+    c.primer_node = NULL;
+    c.ultim_node = NULL;
+    c.longitud = 0;
 }
